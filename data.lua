60954d33272e3192cb9f72a8861395ee35f1502f
Seth Palmer <arcsp@tutamail.com>
2025-04-30 00:01:16 -0600

    switched to following tutorial more closely due to bugs

--- data.lua
+++ data.lua
@@ -1,54 +1,31 @@
---data.lua
+-- These are some style prototypes that the tutorial uses
+-- You don't need to understand how these work to follow along
+local styles = data.raw["gui-style"].default
 
---[[
-
-local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
-
-fireArmor.name = "fire-armor"
-fireArmor.icons = {
-  {
-    icon = fireArmor.icon,
-    icon_size = fireArmor.icon_size,
-    tint = {r=1,g=0,b=0,a=0.3}
-  },
+styles["ugg_content_frame"] = {
+    type = "frame_style",
+    parent = "inside_shallow_frame_with_padding",
+    vertically_stretchable = "on"
 }
 
-fireArmor.resistances = {
-  {
-    type = "physical",
-    decrease = 6,
-    percent = 10
-  },
-  {
-    type = "explosion",
-    decrease = 10,
-    percent = 30
-  },
-  {
-    type = "acid",
-    decrease = 5,
-    percent = 30
-  },
-  {
-    type = "fire",
-    decrease = 0,
-    percent = 100
-  }
+styles["ugg_controls_flow"] = {
+    type = "horizontal_flow_style",
+    vertical_align = "center",
+    horizontal_spacing = 16
 }
 
--- create the recipe prototype from scratch
-local recipe = {
-  type = "recipe",
-  name = "fire-armor",
-  enabled = true,
-  energy_required = 8, -- time to craft in seconds (at crafting speed 1)
-  ingredients = {
-    {type = "item", name = "copper-plate", amount = 200},
-    {type = "item", name = "steel-plate", amount = 50}
-  },
-  results = {{type = "item", name = "fire-armor", amount = 1}}
+styles["ugg_controls_textfield"] = {
+    type = "textbox_style",
+    width = 36
 }
 
-data:extend{fireArmor, recipe}
-
-]]
+styles["ugg_deep_frame"] = {
+    type = "frame_style",
+    parent = "slot_button_deep_frame",
+    vertically_stretchable = "on",
+    horizontally_stretchable = "on",
+    top_margin = 16,
+    left_margin = 8,
+    right_margin = 8,
+    bottom_margin = 4
+}
