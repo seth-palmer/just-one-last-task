869f11bce89e7cc0fce45ca6340a4c4b3e33feca
seth-palmer <arcsp@tutamail.com>
2026-05-29 22:44:40 -0600

    added fn to auto add researched planets for new saves, removed old commented out code

--- control.lua
+++ control.lua
@@ -50,26 +50,28 @@ end
 
 --endregion =======Debug Functions=======
 
+--- For initializing a new save and creating groups for all planets researched
+local function create_groups_for_all_planets_researched()
+    log("initializing planets...")
+    for planet_name in pairs(game.planets) do
+        -- check if researched
+        local tech = game.forces["player"].technologies["planet-discovery-" .. planet_name]
+        if tech and tech.researched then
+            
+            -- create a group for the planet
+            local group_name = Utils.first_to_upper(planet_name)
+            local group_icon = "space-location/" .. planet_name
+            local new_group = {name = group_name, icon = group_icon}
+            Task_manager.add_group(new_group)
+        end
+    end
+end
 
 
-
-
--- Make sure the intro cinematic of freeplay doesn't play every time we restart
--- This is just for convinience, don't worry if you don't understand how this works
+-- Initialize the mod for the first time
 -- See on_init() section in https://wiki.factorio.com/Tutorial:Scripting
 -- Only runs when a new game is created https://lua-api.factorio.com/latest/classes/LuaBootstrap.html#on_init
 script.on_init(function()
-    -- TODO comment out before release
-    -- local freeplay = remote.interfaces["freeplay"]
-    -- if freeplay then -- Disable freeplay popup-message
-    --     if freeplay["set_skip_intro"] then
-    --         remote.call("freeplay", "set_skip_intro", true)
-    --     end
-    --     if freeplay["set_disable_crashsite"] then
-    --         remote.call("freeplay", "set_disable_crashsite", true)
-    --     end
-    -- end
-
     -- Setup default group(s) (store data! not objects/functions)
     -- AVOID using space age specific icons as it will crash in the base game
     local nauvis_group = {id="a1", name="Nauvis", icon="space-location/nauvis"}
@@ -99,6 +101,9 @@ script.on_init(function()
 
     -- setup visual log 
     VisualActionLog.initialize()
+
+    -- add planets if they are researched
+    create_groups_for_all_planets_researched()
 end)
 
 
