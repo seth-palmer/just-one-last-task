4919505d9711060842be1bd98e9786c79830ae11
seth-palmer <arcsp@tutamail.com>
2026-08-06 12:23:59 -0600

    handled edge case for an apia carnova mod with 2 planets under 1 research

--- scripts/events/on_research_finished.lua
+++ scripts/events/on_research_finished.lua
@@ -17,12 +17,29 @@ local OnResearchFinished = {}
 
 --- Adds a group when a new planet is researched
 function OnResearchFinished.add_group_for_new_planet(event)
-
     -- get the name of the technology researched
     local tech = event.research.name
 
+    -- Handle edge case for apia-carnova mod which has 2 planets under 1 research    
+    if script.active_mods["apia"] and tech == 'planet-discovery-apia-carnova' then
+        -- create groups for both planets
+        -- create a group for the planet
+        local group_name = "Apia"
+        local group_icon = "space-location/apia"
+        local new_group = {name = group_name, icon = group_icon}
+        Task_manager.add_group(new_group)
+
+        local group_name = "Carnova"
+        local group_icon = "space-location/carnova"
+        local new_group = {name = group_name, icon = group_icon}
+        Task_manager.add_group(new_group)
+
+        -- Refresh for players 
+        TaskListWindow.refresh_for_all()
+        GroupManagerWindow.refresh_for_all()
+        
     -- if the tech is for a new planet add a group
-    if Utils.string_starts_with(tech, constants.jolt.planet_tech_string_start) then
+    elseif Utils.string_starts_with(tech, constants.jolt.planet_tech_string_start) then
         -- Get the name from the end of the tech research
         local planet_name = tech:gsub(constants.jolt.planet_tech_name_search, "")
 
